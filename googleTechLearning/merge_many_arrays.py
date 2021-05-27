bcb6d71a245145716174d59b2c31b23de6728a20
Laura Varga <laura.varga@walmart.com>
2021-05-27 09:31:10 +0100

    adding merge_multiple_arrays and supporting files

---
+++ googleTechLearning/merge_many_arrays.py
@@ -0,0 +1,62 @@
+import sys
+import heapq
+
+def pick_min_and_advance(array1, array2, pos1, pos2):
+  if pos1 < len(array1) and pos2 < len(array2):
+    if array1[pos1] <= array2[pos2]:
+      result = array1[pos1]
+      pos1 += 1
+    else:
+      result = array2[pos2]
+      pos2 += 1
+  elif pos1 >= len(array1):
+    result = array2[pos2]
+    pos2 += 1
+  else:
+    result = array1[pos1]
+    pos1 += 1
+  return result, pos1, pos2
+
+def merge(array1, array2):
+  pos1 = pos2 = 0
+  result = []
+  while pos1 < len(array1) or pos2 < len(array2):
+    next_value, pos1, pos2 = pick_min_and_advance(array1, array2, pos1, pos2)
+    result.append(next_value)
+
+  return result
+
+def multiple_merge(arrays, n):
+    i = n
+    while i > 0:
+        arrays[0] = merge(arrays[0], arrays[i])
+        i -= 1
+    return arrays[0]
+
+def heap_merge(arrays):
+    result = []
+    for array in arrays:
+        for el in array:
+            heapq.heappush(result, el)
+    return result
+
+file = open("in.txt")
+n = int(file.readline())
+lengths = [int(el) for el in file.readline().split()]
+arrays = [[int(el) for el in file.readline().split()]
+          for length in lengths]
+merged_heap = heap_merge(arrays)
+while len(merged_heap) > 0:
+    sys.stdout.write(str(heapq.heappop(merged_heap)) + ' ')
+
+# merged = multiple_merge(arrays, len(arrays)-1)
+
+# Write here.
+
+# for el in merged:
+#     sys.stdout.write(str(el) + ' ')
+    
+# print("ba")
+# print(multiple_merge([[1, 3], [2, 4, 5], [2, 3, 3, 4]], 2))
+# print(multiple_merge([[1, 2, 3, 4], [2, 3, 4, 5, 6]], 1))
+# print(merge([1, 2, 3, 4], [2, 3, 4, 5, 6]))
